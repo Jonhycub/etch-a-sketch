25bf6e23654519f4fd749c8b80dd0437942611ee
Jonhycub <jonhycub@gmail.com>
2023-12-28 21:50:01 -0500

    Added an event listener so the divs change color
    Now the divs change color whenever the mouse passes over them.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-let numberOfDivs = 16; // Set the number of rows and columns
+let numberOfDivs = 32; // Set the number of rows and columns
 const container = document.querySelector('.container'); //Select the container
 document.documentElement.style.setProperty('--num-divs', numberOfDivs); //Update the number of divs variable in CSS.
 
-
-function createDiv(num){
+//Function that creates the grid of divs.
+function createGrid(num){
   for(let i=0; i<num; i++){
     const divRow = document.createElement('div');
     divRow.classList.add('divRow');
@@ -11,10 +11,13 @@ function createDiv(num){
       const divCol = document.createElement('div');
       divCol.classList.add('divCol');
       container.appendChild(divCol);
+      divCol.addEventListener('mouseover', () =>{   //Event listener to make the divs change color when the mouse is hovered over them
+        divCol.style.backgroundColor = 'grey';
+      })
     }
     container.appendChild(divRow);
   }
 }
 
-createDiv(numberOfDivs);
+createGrid(numberOfDivs); //Call the function to create the grid base on the number of rows and columns
 
