a57a5e9054cb42a737d032f55983386bfb859c03
Jonhycub <jonhycub@gmail.com>
2023-12-28 21:30:00 -0500

    Finished making the grid
    Now the grid is done, it also updates the size of the divs according to the amount.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
-let numberOfDivs = Number(prompt("How many rows?"));
-const container = document.querySelector('.container');
+let numberOfDivs = 16; // Set the number of rows and columns
+const container = document.querySelector('.container'); //Select the container
+document.documentElement.style.setProperty('--num-divs', numberOfDivs); //Update the number of divs variable in CSS.
 
 
 function createDiv(num){
-  for(let i=0; i<numberOfDivs; i++){
+  for(let i=0; i<num; i++){
     const divRow = document.createElement('div');
     divRow.classList.add('divRow');
+    for(let j=0; j<num; j++){
+      const divCol = document.createElement('div');
+      divCol.classList.add('divCol');
+      container.appendChild(divCol);
+    }
     container.appendChild(divRow);
   }
 }
 
-// createDiv(numberOfDivs);
+createDiv(numberOfDivs);
 
-for(let i=0; i<numberOfDivs; i++){
-  createDiv(numberOfDivs);
-}
